5e043801468aea085250b3d51a0f051e8784df87
Tu Do Van <tudv@UNIT.COM>
2019-08-20 11:08:24 +0700

    chay tren window

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from "react-redux";
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store from './store'
+import ContactForm from './src/component/redux/contactForm'
+// export default class App extends Compon
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/component/redux/contactForm.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native'
+
+const renderField = ({ label, keyboardType, name }) => {
+    return (
+        <View style={{ flexDirection: 'row', height: 58, alignItems: 'center', }}>
+            <Text>{label}</Text>
+            <TextInput keyboardType={keyboardType}></TextInput>
+        </View>);
+};
+const ContactComponent = props => {
+    const { handelSubmit } = props;
+    return (
+        <View>
+            <Text>redux-form example</Text>
+            <Field keyboardType="default" label="Username:" component={renderField} name="Username" />
+            <Field keyboardType="email-address" label="Email:" component={renderField} name="Email" />
+            <TouchableOpacity onPress={handelSubmit} >
+                <Text>Submit</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+const ContactForm = reduxForm({
+    form: 'contact', // a unit name for only this form
+})(ContactComponent)
+export default ContactForm
+
+
+
+
+// class ContactComponent extends Component{
+//     render(){
+//         const { handelSubmit} = this.props;
+//         return (
+//             <View></View>
+//         )
+//     }
+// }
+
+
+
+
+
+
+// let ContactForm = props => {
+//     const { handleSubmit } = props
+//     return <form onSubmit={handleSubmit}>{/* form body*/}</form>
+// }
+
+// ContactForm = reduxForm({
+//     // ten cua moi form la duy nhat
+//     form: 'contact'
+// })(ContactForm)
+
+// export default ContactForm
